31e1ddb54029a28c17d499536e5b20a81e393e6f
Corey Pierce <coreypierce11@gmail.com>
2025-03-15 11:14:53 -0400

    Begin tokenizing character

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,68 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-    println!("EOF");
+    println!("End");
     Ok(())
 }
 
-// TODO: return result indicating success
-fn tokenize_line(mut line: &String) {
-    println!("{line}");
+#[derive(Debug)]
+struct Token {
+    text: String, // used for identifiers, strings, numbers
+    token_type: TokenType 
 }
 
+// TODO: return result indicating successor
+// Here's our tokenizer. Function for now, can change later if necessary.
+fn tokenize_line(line: &String) {
+    let mut tokens: Vec<Token> = Vec::new();
+    for curr in line.chars() {
+        let token = match curr {
+            '+' => Token { token_type: TokenType::Plus, text: String::from(curr) },
+            '-' => Token { token_type: TokenType::Minus, text: String::from(curr) },
+            '*' => Token { token_type: TokenType::Asterisk, text: String::from(curr) },
+            '/' => Token { token_type: TokenType::Slash, text: String::from(curr) },
+            _ => Token { token_type: TokenType::UnsupportedSymbolError, text: String::from(curr) }
+        };
+    
+       tokens.push(token); 
+    }
 
+    println!("{:#?}", tokens);
+}
 
+//fn create_token(tokenType: TokenType, text: 
+
+#[derive(Debug)]
+#[allow(dead_code)]
+enum TokenType {
+    EOF,
+    Newline,
+    Number,
+    Identity,
+    Str,
+    // Keywords
+    Label,
+    Goto,
+    Print,
+    Input,
+    Let,
+    If,
+    Then,
+    EndIf,
+    While,
+    Repeat,
+    EndWhile,
+    // Operators
+    Equal,  
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    EqualEqual,
+    NotEqual,
+    LessThan,
+    LessThanEqualTo,
+    GreaterThan,
+    GreaterThanEqualTo,
+    UnsupportedSymbolError
+}
