dd0e594634c9d41096f817c1ee183937f3b8d2e6
Corey Pierce <coreypierce11@gmail.com>
2025-03-15 12:03:36 -0400

    Tokenize ==
    Ended up changing loop to be slightly different since we must start peaking at the next characters in the line

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() -> std::io::Result<()> {
     
     for line_result in reader.lines() {
         if let Ok(line_str) = line_result {
-            tokenize_line(&line_str);         
+            tokenize_line(line_str);         
         }
     }
 
@@ -26,18 +26,39 @@ struct Token {
 
 // TODO: return result indicating successor
 // Here's our tokenizer. Function for now, can change later if necessary.
-fn tokenize_line(line: &String) {
+fn tokenize_line(line: String) {
     let mut tokens: Vec<Token> = Vec::new();
-    for curr in line.chars() {
+    let line_bytes: &[u8] = line.as_bytes();
+    let mut curr_byte_index = 0;
+
+    //for curr in line.chars() {
+    //for mut i in 0..line_bytes.len() {
+    while curr_byte_index < line_bytes.len() {
+        let curr = line_bytes[curr_byte_index];
+        let curr = curr as char;
+        println!("{}", curr);
         let token = match curr {
             '+' => Token { token_type: TokenType::Plus, text: String::from(curr) },
             '-' => Token { token_type: TokenType::Minus, text: String::from(curr) },
             '*' => Token { token_type: TokenType::Asterisk, text: String::from(curr) },
             '/' => Token { token_type: TokenType::Slash, text: String::from(curr) },
+            '=' => {
+                let next = line_bytes[curr_byte_index+1] as char;
+                if next == '=' {
+                    curr_byte_index += 1;
+                    Token { token_type: TokenType::EqualEqual, text: String::from("==") }
+                } else {
+                    Token { token_type: TokenType::Equal, text: String::from("=") }
+                }
+            },
             _ => Token { token_type: TokenType::UnsupportedSymbolError, text: String::from(curr) }
         };
-    
-       tokens.push(token); 
+
+        curr_byte_index += 1;
+   
+        if token.token_type != TokenType::UnsupportedSymbolError {  
+            tokens.push(token); 
+        }
     }
 
     println!("{:#?}", tokens);
@@ -46,6 +67,7 @@ fn tokenize_line(line: &String) {
 //fn create_token(tokenType: TokenType, text: 
 
 #[derive(Debug)]
+#[derive(PartialEq)]
 #[allow(dead_code)]
 enum TokenType {
     EOF,
@@ -77,5 +99,7 @@ enum TokenType {
     LessThanEqualTo,
     GreaterThan,
     GreaterThanEqualTo,
-    UnsupportedSymbolError
+    UnsupportedSymbolError,
+    // Won't get through to the parser, just for processing in here.
+    Space
 }
