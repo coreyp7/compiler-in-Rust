9ca78eeda8d80ed68d22181f43b6b49cf8327a64
Corey Pierce <coreypierce11@gmail.com>
2025-10-10 12:27:13 -0400

    Add logic for Value to be a function call (with params)

--- src/ast/pure_builder.rs
+++ src/ast/pure_builder.rs
@@ -65,11 +65,7 @@ fn parse_variable_declaration(mut context: BuilderContext) -> (Statement, Builde
             symbol_name: String::new(),
             data_type: DataType::Invalid,
             line_declared_on: 0,
-            assigned_value: Value {
-                data_type: DataType::Invalid,
-                value_type: ValueType::Invalid,
-                raw_text: String::new(),
-            },
+            assigned_value: Value::invalid(),
         });
         return (invalid_stmt, context);
     }
@@ -85,11 +81,7 @@ fn parse_variable_declaration(mut context: BuilderContext) -> (Statement, Builde
             symbol_name,
             data_type: DataType::Invalid,
             line_declared_on,
-            assigned_value: Value {
-                data_type: DataType::Invalid,
-                value_type: ValueType::Invalid,
-                raw_text: String::new(),
-            },
+            assigned_value: Value::invalid(),
         });
         return (invalid_stmt, context);
     }
@@ -201,47 +193,110 @@ fn parse_return_statement(mut context: BuilderContext) -> (Statement, BuilderCon
     (statement, context)
 }
 
-/// Parse a value expression - no validation, just structure
+// Parse a value expression - no validation, just structure
 fn parse_value(mut context: BuilderContext) -> (Value, BuilderContext) {
     if context.is_at_end() {
-        let invalid_value = Value {
-            data_type: DataType::Invalid,
-            value_type: ValueType::Invalid,
-            raw_text: String::new(),
-        };
-        return (invalid_value, context);
+        return (Value::invalid(), context);
     }
 
-    let token = context.get_curr();
+    let token = context.get_curr().clone();
     let value = match token.token_type {
-        TokenType::Number => Value {
-            data_type: DataType::Number,
-            value_type: ValueType::InlineNumber,
-            raw_text: token.lexeme.clone(),
-        },
-        TokenType::Str => Value {
-            data_type: DataType::String,
-            value_type: ValueType::InlineString,
-            raw_text: token.lexeme.clone(),
-        },
+        TokenType::Number => Value::new(
+            DataType::Number,
+            ValueType::InlineNumber,
+            token.lexeme.clone(),
+        ),
+        TokenType::Str => Value::new(
+            DataType::String,
+            ValueType::InlineString,
+            token.lexeme.clone(),
+        ),
         TokenType::Identity => {
             // For now, treat all identities as variable references
             // Semantic analysis will determine if they're valid
+            /*
             Value {
                 data_type: DataType::Unknown, // Will be determined in semantic analysis
                 value_type: ValueType::Variable,
                 raw_text: token.lexeme.clone(),
             }
+            */
+            // TODO: update this to allow function calls.
+            // Indicate its a function call in the AST node somehow, then check
+            // for this in the semantic analysis and check appropriate table.
+
+            /*
+             * if just variable name (no parameters), treat as variable.
+             * if identity name with parameters, treat as function.
+             * TODO: we're gonna need some vars to keep track of possible symbols
+             * passed into the param of the possible function call this value can be.
+             * Actually, we could store *these* as values, since a passed param can
+             * be a Value in itself. This starts a possible recursive process.
+             *
+             * - add new var that's a vector of values
+             * - process each argument "value" and add to list
+             * Reminder: DO NOT have any validation logic or semantic analysis in here.
+             *
+             * The only thing I think that could go wrong here is the parsing of each
+             * of the values; how does it know when to stop, will it behave normally?
+             * I guess we'll see.
+             */
+            // Check if this is a function call
+            if let Some(next_token) = context.peek_next() {
+                if next_token.token_type == TokenType::LeftParen {
+                    // This is a function call - we need to handle the parameter parsing
+                    // but we'll do it after we advance past the current token
+                    // For now, just mark it as a function call and let the calling code handle parameters
+                    let raw_text = context.get_curr().lexeme.clone();
+                    context.advance();
+                    context.advance(); // move passed (
+
+                    let (params, ctx) = parse_function_call_parameters(context);
+                    context = ctx;
+                    // Context moves passed closing paren after this match; at bottom
+                    // of function we advance.
+
+                    Value::new_with_params(
+                        DataType::Unknown,
+                        ValueType::FunctionCall,
+                        raw_text,
+                        params,
+                    )
+                } else {
+                    // create Value as a variable call
+                    Value::new(DataType::Unknown, ValueType::Variable, token.lexeme.clone())
+                }
+            } else {
+                // actually could just be final statement of the file...
+                // should test this TODO: treat as variable for now
+                Value::new(DataType::Unknown, ValueType::Variable, token.lexeme.clone())
+            }
         }
-        _ => Value {
-            data_type: DataType::Invalid,
-            value_type: ValueType::Invalid,
-            raw_text: token.lexeme.clone(),
-        },
+        _ => Value::new(DataType::Invalid, ValueType::Invalid, token.lexeme.clone()),
     };
 
     context.advance();
     (value, context)
+}
+
+// Called when a function call is found, gathers all values specified in a function
+// calls parameters.
+fn parse_function_call_parameters(mut context: BuilderContext) -> (Vec<Value>, BuilderContext) {
+    // so it'll be value comma value etc....
+
+    let mut passed_values: Vec<Value> = Vec::new();
+
+    while context.get_curr().token_type != TokenType::RightParen {
+        let (value, ctx) = parse_value(context);
+        context = ctx;
+        passed_values.push(value);
+
+        if context.get_curr().token_type == TokenType::Comma {
+            context.advance();
+        }
+    }
+
+    (passed_values, context)
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -268,4 +323,36 @@ pub struct Value {
     pub data_type: DataType,
     pub value_type: ValueType,
     pub raw_text: String, // The raw text from the source, for reference
+    // Only exists if value_type = FunctionCall; we need to record the values
+    // being passed in as params.
+    pub param_values: Option<Vec<Value>>,
+}
+
+impl Value {
+    pub fn new(data_type: DataType, value_type: ValueType, raw_text: String) -> Self {
+        Value {
+            data_type,
+            value_type,
+            raw_text,
+            param_values: None,
+        }
+    }
+
+    pub fn new_with_params(
+        data_type: DataType,
+        value_type: ValueType,
+        raw_text: String,
+        param_values: Vec<Value>,
+    ) -> Self {
+        Value {
+            data_type,
+            value_type,
+            raw_text,
+            param_values: Some(param_values),
+        }
+    }
+
+    pub fn invalid() -> Self {
+        Value::new(DataType::Invalid, ValueType::Invalid, String::new())
+    }
 }
